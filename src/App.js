11719df8d1a4b6b1f5c87d7db41e30ca1a7cf890
django-json <diango.jayson@gmail.com>
2022-03-02 15:52:33 +0800

    add useMediaQuery custom hook to render different components based on screen size

--- src/App.js
+++ src/App.js
@@ -6,7 +6,26 @@ import { ReactComponent as Logo } from './assets/images/catwikilogo.svg';
 
 import Input from './components/input/input.component';
 
+import { useMediaQuery } from './custom-hooks/useMediaQuery';
+
 function App() {
+    const mobileView = useMediaQuery('(max-width: 767.98px)');
+
+    function shouldRenderSearchBox() {
+        return mobileView ? (
+            <div className="App__search-static">
+                <span>Search</span>
+                <i className="material-icons">search</i>
+            </div>
+        ) : (
+            <Input
+                type="text"
+                placeholder="Enter your breed"
+                endIcon="search"
+                size="lg"
+            />
+        );
+    }
     return (
         <div className="App">
             <Logo />
@@ -14,12 +33,7 @@ function App() {
                 <div className="App__row">
                     <Logo className="App__logo" />
                     <p>Get to know more about your cat breed</p>
-                    <Input
-                        type="text"
-                        placeholder="Enter your breed"
-                        endIcon="search"
-                        size="lg"
-                    />
+                    {shouldRenderSearchBox()}
                 </div>
             </div>
         </div>
