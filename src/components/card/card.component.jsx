4c48b4226c85841334aea1751086af810083c721
django-json <diango.jayson@gmail.com>
2022-03-09 09:58:33 +0800

    refactor card component and styles

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,22 +4,14 @@ import './card.styles.css';
 
 const defaultProps = {
     photoURL:
-        'https://media.istockphoto.com/photos/bengal-cat-at-home-picture-id1276717883?b=1&k=20&m=1276717883&s=170667a&w=0&h=BPuk46_p--l8RQDAk-EXLDMe-03pyq30jbBQ4pfx-YY=',
-    width: '220px',
-    height: '220px'
+        'https://media.istockphoto.com/photos/bengal-cat-at-home-picture-id1276717883?b=1&k=20&m=1276717883&s=170667a&w=0&h=BPuk46_p--l8RQDAk-EXLDMe-03pyq30jbBQ4pfx-YY='
 };
 
-function Card({ photoURL, title, width, height }) {
+function Card({ photoURL, title }) {
     return (
         <div className="card">
             <div className="card__image-container">
-                <img
-                    className="card__image"
-                    src={photoURL}
-                    alt={title}
-                    width={width}
-                    height={height}
-                />
+                <img className="card__image" src={photoURL} alt="Cat Breed" />
             </div>
             <div className="card__title">{title}</div>
         </div>
