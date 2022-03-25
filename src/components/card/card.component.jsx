6891a88ac8555031aed875a9181569fc5cecdaa9
django-json <diango.jayson@gmail.com>
2022-03-25 15:29:32 +0800

    refactor card component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -7,9 +7,9 @@ const defaultProps = {
         'https://media.istockphoto.com/photos/bengal-cat-at-home-picture-id1276717883?b=1&k=20&m=1276717883&s=170667a&w=0&h=BPuk46_p--l8RQDAk-EXLDMe-03pyq30jbBQ4pfx-YY='
 };
 
-function Card({ photoURL, title }) {
+function Card({ photoURL, title, ...props }) {
     return (
-        <div className="card">
+        <div className="card" {...props}>
             <div className="card__image-container">
                 <img className="card__image" src={photoURL} alt="Cat Breed" />
             </div>
