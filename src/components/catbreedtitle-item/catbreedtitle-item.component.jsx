e8bedc18ec099f9756a5f32669fc5c2246f72c55
django-json <diango.jayson@gmail.com>
2022-03-25 15:33:59 +0800

    navigate user to page when component is click

--- src/components/catbreedtitle-item/catbreedtitle-item.component.jsx
+++ src/components/catbreedtitle-item/catbreedtitle-item.component.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './catbreedtitle-item.styles.css';
 
 function CatBreedTitleItem({ item }) {
-    return <li className="catbreedtitle-item">{item}</li>;
+    const navigate = useNavigate();
+    function handleClick() {
+        navigate(`breed-details/${item.id}`);
+    }
+    return (
+        <li className="catbreedtitle-item" onClick={handleClick}>
+            {item.name}
+        </li>
+    );
 }
 
 CatBreedTitleItem.displayName = 'CatBreedTitleItem';
