36c601cdd02d7b1cde9b2502bd392405d1508d19
django-json <diango.jayson@gmail.com>
2022-03-25 15:41:42 +0800

    refactor component

--- src/components/mostsearchedbreeds-preview/mostsearchedbreeds-preview.component.jsx
+++ src/components/mostsearchedbreeds-preview/mostsearchedbreeds-preview.component.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './mostsearchedbreeds-preview.styles.css';
 
 import Card from '../card/card.component';
 
+import { data } from '../../data';
+
 function MostSearchedBreedsPreview() {
+    const navigate = useNavigate();
+    function handleCardClick(id) {
+        navigate(`breed-details/${id}`);
+    }
+
     return (
         <div className="mostsearchedbreeds-preview">
-            <Card title="Bengal" />
-            <Card title="Savannah" />
-            <Card title="Norweigan Forest Cat" />
-            <Card title="Selkirk Rex" />
+            {data.map((item) => {
+                return (
+                    <Card
+                        key={item.id}
+                        title={item.name}
+                        photoURL={item.image.url}
+                        onClick={() => handleCardClick(item.id)}
+                    />
+                );
+            })}
         </div>
     );
 }
