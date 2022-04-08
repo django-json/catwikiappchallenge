23bd77d90b40e72dc9fe5f765164b262208459aa
django-json <diango.jayson@gmail.com>
2022-04-08 15:30:04 +0800

    finalize App component's layout and routes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
 import { ReactComponent as Logo } from './assets/images/catwikilogo.svg';
+import Footer from './components/footer/footer.component';
+import CustomLink from './components/custom-link/custom-link.component';
+import ScrollToTop from './components/scroll-to-top/scroll-to-top.component';
 
-import Input from './components/input/input.component';
-
-import { useMediaQuery } from './custom-hooks/useMediaQuery';
+import BreedDetailsPageContainer from './pages/breed-details/breed-details.container';
+import MostSearchedBreedsPageContainer from './pages/mostsearchedbreeds/mostsearchedbreeds.container';
+import HomePage from './pages/home/home.component';
 
 function App() {
-    const mobileView = useMediaQuery('(max-width: 767.98px)');
-
-    function shouldRenderSearchBox() {
-        return mobileView ? (
-            <div className="App__search-static">
-                <span>Search</span>
-                <i className="material-icons">search</i>
-            </div>
-        ) : (
-            <Input
-                type="text"
-                placeholder="Enter your breed"
-                endIcon="search"
-                size="lg"
-            />
-        );
-    }
     return (
         <div className="App">
-            <Logo />
-            <div className="App__col">
-                <div className="App__row">
-                    <Logo className="App__logo" />
-                    <p>Get to know more about your cat breed</p>
-                    {shouldRenderSearchBox()}
-                </div>
-            </div>
+            <CustomLink to=".">
+                <Logo className="App__logo" />
+            </CustomLink>
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route
+                    path="mostsearchedbreeds"
+                    element={<MostSearchedBreedsPageContainer />}
+                />
+                <Route
+                    path="breed-details/:id"
+                    element={<BreedDetailsPageContainer />}
+                />
+            </Routes>
+            <Footer />
         </div>
     );
 }
