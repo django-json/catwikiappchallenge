5e4657b8d2a70bb9e71b76c45b4b0973684c29b6
django-json <diango.jayson@gmail.com>
2022-04-08 15:09:53 +0800

    add spinner when fetching data

---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './spinner.styles.css';
+
+function Spinner() {
+    return (
+        <div className="spinner">
+            <i className="material-icons">sync</i>
+        </div>
+    );
+}
+
+export default Spinner;
